84e62058aad64b357479350e47360c81186194eb
icyice777 <69426230+icyice777@users.noreply.github.com>
2026-05-20 17:25:50 +0000

    Update content

--- scripts/renew_iptv.sh
+++ scripts/renew_iptv.sh
@@ -4,4 +4,9 @@
 echo "::notice::The script started successfully."
 
 # Download m3u file
-curl https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8 > ../iptv.m3u
+curl https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8 > iptv.m3u
+echo "::notice::iptv.m3u has been downloaded successfully."
+
+# Encrypt iptv.m3u file
+openssl enc -in iptv.m3u -out iptv.enc.m3u -e -$algorithm -iv $ivHex -pbkdf2 -pass pass:$password -S $saltHex -iter $iterations -md $diggest
+echo "::notice::iptv.m3u has been encrypted successfully."
