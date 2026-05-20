f8590cfb78770e0d9d9603d228977a71dc97ae88
icyice777 <69426230+icyice777@users.noreply.github.com>
2026-05-20 16:59:44 +0000

    Update content

--- scripts/renew_iptv.sh
+++ scripts/renew_iptv.sh
@@ -2,3 +2,6 @@
 
 # Creates a blue notice icon in the logs
 echo "::notice::The script started successfully."
+
+# Download m3u file
+curl https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8 > ../iptv.m3u
