6d7c45b353462dd7311aed7180f516564f2ddd39
icyice777 <69426230+icyice777@users.noreply.github.com>
2026-05-22 06:37:06 +0000

    Update content

--- scripts/renew_iptv.sh
+++ scripts/renew_iptv.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-# Creates a blue notice icon in the logs
-echo "::notice::The script started successfully."
+echo "::notice::Download $original_file"
+curl -A "$ua" "$original_file_url" > $original_file
 
-# Download m3u file
-curl https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8 > iptv.m3u
-echo "::notice::iptv.m3u has been downloaded successfully."
-
-# Encrypt iptv.m3u file
-openssl enc -in iptv.m3u -out iptv.enc.m3u -e -$algorithm -iv $ivHex -pbkdf2 -pass pass:$password -S $saltHex -iter $iterations -md $diggest
-echo "::notice::iptv.m3u has been encrypted successfully."
+if [ -e $original_file ]
+then
+   if [ "$(stat -c%s $original_file)" -gt "$smallest_file_size" ] && grep -q $test_channel $original_file
+   then
+      echo "::notice::Move MCP channels of cctv, 卫视, and channels of 测试 to $temp_file"
+      sed -n '1p' $original_file > $temp_file
+      sed -n '/cctv\([0-9]\+\)\([p]\?\)-MCP$/,/https:/p' $original_file >> $temp_file
+      sed -n '/卫视-MCP$/,/https:/p' $original_file >> $temp_file
+      sed -n '/五星体育/,/https:/p' $original_file >> $temp_file
+      sed -n '/测试$/,/https:/p' $original_file >> $temp_file
+      echo "::notice::Add tvg-id to $temp_file for enabling EPG"
+      sed -i -E 's/tvg\-name=\"cctv[0-9]+[a-z]*\"/& &/' $temp_file
+      LANG=C sed -i -E 's/tvg\-name=\"[\x81-\xFE]+\"/& &/' $temp_file
+      sed -i -E 's/tvg\-name=\"4K60PSDR-H264-AAC测试\"/& &/' $temp_file
+      sed -i -E 's/tvg\-name=\"4K60PHLG-HEVC-EAC3测试\"/& &/' $temp_file
+      sed -i 's/\-1 tvg-name/\-1 tvg-id/' $temp_file
+      echo "::notice::Encrypt $temp_file as $target_file"
+      openssl enc -in $temp_file -out $target_file -e -$algorithm -iv $ivHex -pbkdf2 -pass pass:$password -S $saltHex -iter $iterations -md $diggest
+      echo "::notice::Rmove $temp_file"
+      rm $temp_file
+   else
+      echo "::warning::Either $original_file is smaller than $smallest_file_size or $test_channel was not found in $original_file, skip handling"
+      exit 1
+   fi
+   echo "::notice::Rmove $original_file"
+   rm $original_file
+else
+   echo "::error::Download failed"
+   exit 1
+fi
